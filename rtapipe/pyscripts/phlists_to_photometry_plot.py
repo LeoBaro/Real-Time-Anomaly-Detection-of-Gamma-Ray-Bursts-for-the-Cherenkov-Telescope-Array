c1f10ec59baff7d5bc7a6bf326eaf704ebe109a4
LeoBaro <leonardo.baroncelli@inaf.it>
2021-03-03 18:23:09 +0100

    AP plots notebook

--- rtapipe/pyscripts/phlists_to_photometry_plot.py
+++ rtapipe/pyscripts/phlists_to_photometry_plot.py
@@ -11,7 +11,7 @@ import string
 
 from rtapipe.pyscripts.data_utils import DataUtils
 
-plt.rcParams.update({'font.size': 10})
+plt.rcParams.update({'font.size': 7, 'lines.markersize': 0.5,'legend.markerscale': 5, 'lines.linewidth':0.5, 'lines.linestyle':'--'})
 
 class PhotometryPlot(ABC):
     
@@ -26,13 +26,16 @@ class PhotometryPlot(ABC):
 
     def getData(self, photometry_csv_file, integration):
         df = pd.read_csv(photometry_csv_file)
+        # print(df.head(10))
         if integration == "t" or integration=="e": 
             return {
                 "x": df['VALCENTER'],
                 "y": df['COUNTS'],
-                "err": df['ERROR']
+                "err": df['ERROR'],
+                "valmax": df['VALMAX'],
+                "valmin": df['VALMIN']
             }
-        if integration == "t,e": 
+        if integration == "t+e": 
             return {
                 "x1": df['TMID'],
                 "x2": df['EMID'],
@@ -66,19 +69,36 @@ class PhotometrySinglePlot(PhotometryPlot):
 
     def addData(self, photometry_csv_file, args, label_on, integration):
         data = super().getData(photometry_csv_file, integration)
+        # print(f"\naddData called!")
+
+        label_on_string = ""
+        for label in label_on:
+            if label in args:
+                label_on_string += label+"_"+str(args[label])+"_"
+            else:
+                label_on_string += label
+
+        if integration == "t":
+            _ = self.FSX.scatter(data["x"], data["y"], s=0.1, label=label_on_string, color=PhotometryPlot.colors[self.ccount])
+            _ = self.FSX.errorbar(data["x"], data["y"], yerr=data["err"], fmt="o", color=PhotometryPlot.colors[self.ccount]) 
+            _ = self.FSX.axvline(x=args["onset"], color="red", linestyle="--")
+
+        elif integration == "e":
+            _ = self.FSX.bar(data["x"], data["y"], yerr=data["err"], label=label_on_string , color=PhotometryPlot.colors[self.ccount], alpha=0.1, width=args["e_window_step"])
+
         
-        _ = self.FSX.scatter(data["x"], data["y"], label=label_on+"_"+str(args[label_on]), color=PhotometryPlot.colors[self.ccount])
-        _ = self.FSX.errorbar(data["x"], data["y"], yerr=data["err"], fmt="o", color=PhotometryPlot.colors[self.ccount]) 
-        _ = self.FSX.axvline(x=args["onset"], color="red", linestyle="--")
         self.ccount += 1
 
-        forbidden = self.getForbidden() + ["label_on"] 
-        ok = ["pointing", "regios_radius", "t_window_start", "t_window_size", "t_window_step", "onset"]
+        forbidden = self.getForbidden() + label_on
+        ok = ["pointing", "region_radius", "t_window_start", "t_window_size", "t_window_step", "t_window_stop", "e_window_start", "e_window_size", "e_window_step", "e_window_stop", "onset"]
 
-        title = f'Aperture Photometry Counts: \n {[f"{key}={value} " for key, value in args.items() if key not in forbidden and key in ok]}'
+        kawrgs = [f"{key}: {value} " for key, value in args.items() if key not in forbidden and key in ok]
+        title = f'Aperture Photometry Counts: \n'
+        title += str(kawrgs[:5]) + "\n" + str(kawrgs[5:])
+
         self.FSX.set_title(title)
-        self.FSX.set_ylabel('Windowed Counts')
-        self.FSX.set_xlabel(f'Window center')
+        self.FSX.set_ylabel('Counts')
+        self.FSX.set_xlabel(f'Window center (integration: "{integration}")')
         self.FSX.legend(loc="best")
 
     def show(self):
@@ -94,38 +114,93 @@ class PhotometrySubPlots(PhotometryPlot):
 
     def __init__(self):
         self.FM, self.FMX  = plt.subplots(2, 2)
-        self.FMi, self.FMj, self.FMc = 0, 0, 0  
         self.FM.set_size_inches(PhotometryPlot.inch_x, PhotometryPlot.inch_y)
+        self.current_axis_idx = 0
+        self.axes = [(0,0), (0,1), (1,0), (1,1)]
 
+    def nextAxis(self):
+        self.current_axis_idx = (self.current_axis_idx + 1) % len(self.axes)
+        self.ccount = (self.ccount + 1) % len(self.axes)
+        x,y = self.axes[self.current_axis_idx]
+        return self.FMX[x][y]
+    
+    def prevAxis(self):
+        self.current_axis_idx = (self.current_axis_idx - 1) % len(self.axes)
+        self.ccount = (self.ccount - 1) % len(self.axes)
+        x,y = self.axes[self.current_axis_idx]
+        return self.FMX[x][y]
 
-    def addData(self, photometry_csv_file, args, label_on, integration):
+    def currentAxis(self):
+        x,y = self.axes[self.current_axis_idx]     
+        return self.FMX[x][y]
+
+    def addData(self, photometry_csv_file, args, label_on, integration, as_baseline=False):
         data = super().getData(photometry_csv_file, integration)
+        # print(f"\naddData called! as_baseline={as_baseline} ")
 
-        axis = self.FMX[self.FMi][self.FMj]
-
-        _ = axis.scatter(data["x"], data["y"], label=label_on+"_"+str(args[label_on]), color=PhotometryPlot.colors[self.ccount])
-        _ = axis.errorbar(data["x"], data["y"], yerr=data["err"], fmt="o", color=PhotometryPlot.colors[self.ccount])
-        _ = axis.axvline(x=args["onset"], color="red", linestyle="--")
-        self.ccount += 1
-
-        print(f"FMi: {self.FMi} FMj: {self.FMj}")
-        if self.FMc % 2 == 0:
-            self.FMi = (self.FMi + 1) % 2
+        if as_baseline:
+            axis = self.prevAxis()
         else:
-            self.FMj = (self.FMj + 1) % 2
-        print(f"FMi: {self.FMi} FMj: {self.FMj}")
-        self.FMc+=1
+            axis = self.currentAxis()
+            
+        #print("---  current axis: ", self.current_axis_idx)
 
-        forbidden = self.getForbidden() + ["label_on"] 
-        ok = ["pointing", "regios_radius", "t_window_start", "t_window_size", "t_window_step", "onset"]
+        label_on_string = ""
+        for label in label_on:
+            if label in args:
+                label_on_string += label+"_"+str(args[label])+"_"
+            else:
+                label_on_string += label
 
-        title = f'Aperture Photometry Counts: \n {[f"{key}={value} " for key, value in args.items() if key not in forbidden and key in ok ]}'
+        if integration == "t":
+
+            if not as_baseline: 
+                plotargs = {"color": PhotometryPlot.colors[self.ccount]}
+            else:
+                plotargs = {"color": "black"}
+
+            if not as_baseline: 
+                _ = axis.scatter(data["x"], data["y"], label = label_on_string, **plotargs, s=0.1)
+                _ = axis.errorbar(data["x"], data["y"], yerr=data["err"], fmt="o", **plotargs)
+                _ = axis.axvline(x=args["onset"], color="red", linestyle="--")
+            else:
+                _ = axis.scatter(data["x"], data["y"], label = "baseline", **plotargs, s=0.1)
+                _ = axis.errorbar(data["x"], data["y"], yerr=data["err"], fmt="o", **plotargs)
+
+
+        elif integration == "e":
+            plotargs = {
+                "yerr": data["err"], 
+                "label": label_on_string,
+                "alpha": 0.2, 
+                "width": args["e_window_step"]
+            }
+            if as_baseline:
+                plotargs["linewidth"] = 4
+                plotargs["color"] = "black"
+            else:
+                plotargs["color"] = PhotometryPlot.colors[self.ccount]
+
+            _ = axis.bar(data["x"], data["y"], **plotargs)
+            
+
+        forbidden = self.getForbidden() + label_on 
+        ok = ["pointing", "region_radius", "t_window_start", "t_window_size", "t_window_step", "t_window_stop", "e_window_start", "e_window_size", "e_window_step", "e_window_stop", "onset"]
+
+        kawrgs = [f"{key}: {value} " for key, value in args.items() if key not in forbidden and key in ok]
+        title = f'Aperture Photometry Counts: \n'
+        title += str(kawrgs[:5]) + "\n" + str(kawrgs[5:])
+
         self.FM.suptitle(title)
-        axis.set_title(f"{label_on}: {args[label_on]}")
-        axis.set_ylabel('Windowed Counts')
-        axis.set_xlabel(f'Window center')
-                
-        self.FM.tight_layout()
+        axis.set_title(label_on_string)
+        axis.set_ylabel('Counts')
+        axis.set_xlabel(f'Window center (integration: "{integration}")')
+
+        axis.legend(loc="best")
+
+        self.nextAxis()
+
+        # self.FM.tight_layout()
 
     def save(self, outfileName):
         outfileName = outfileName+'.png'
@@ -141,15 +216,14 @@ class Photometry:
 
     def getWindows(self, window_start, window_stop, window_size, window_step):
 
-        print(f"window_start {window_start}, window_stop {window_stop}, window_size {window_size}, window_step {window_step}")
+        #print(f"window_start {window_start}, window_stop {window_stop}, window_size {window_size}, window_step {window_step}")
         if window_size == 0:
             raise ValueError("window-size must be greater than zero.")
 
         _windows = []
 
-        t_start = window_start
         t_stop = window_stop
-        w_start = 0
+        w_start = window_start
         while w_start + window_size <= t_stop:
             _windows.append((w_start, w_start + window_size))
             w_start += window_step
@@ -164,27 +238,28 @@ class Photometry:
         return result_str
 
 
-    def generate(self, args, integration):
+    def integrate(self, photometry_params, sim_params, integration):
+        # print(f"\ngenerate() has been called! integration={integration}")
+        
+        runid = sim_params["runid"]  
+        simtype = sim_params["simtype"]  
+        region_radius = photometry_params["region_radius"]
 
-        runid = args["runid"]  
-        simtype = args["simtype"]  
-        region_radius = args["region_radius"]
-
-        time_windows = self.getWindows(args["t_window_start"], args["t_window_stop"], args["t_window_size"], args["t_window_step"])
-        energy_windows = self.getWindows(args["e_window_start"], args["e_window_stop"], args["e_window_size"], args["e_window_step"])
+        time_windows = self.getWindows(0, sim_params["t_window_stop"], photometry_params["t_window_size"], photometry_params["t_window_step"])
+        energy_windows = self.getWindows(sim_params["e_window_start"], sim_params["e_window_stop"], photometry_params["e_window_size"], photometry_params["e_window_step"])
 
         #print(f"time_windows: {time_windows}")
-        print(f"energy_windows: {energy_windows}")
+        #print(f"energy_windows: {energy_windows}")
         
         
         input_file = None
         if simtype == 'grb':
-            input_file = os.path.join(args["obs_dir"], runid, "ebl000001.fits")
-            output_dir = os.path.join(args["obs_dir"], runid, "ap")
+            input_file = os.path.join(sim_params["obs_dir"], runid, "ebl000001.fits")
+            output_dir = os.path.join(sim_params["obs_dir"], runid, "ap")
             output_file = os.path.join(output_dir, f"ebl000001_ap_mode")   
         elif simtype == 'bkg': # bkg
-            input_file = os.path.join(args["obs_dir"], "backgrounds", "bkg000001.fits")
-            output_dir = os.path.join(args["obs_dir"], "backgrounds", "ap")
+            input_file = os.path.join(sim_params["obs_dir"], "backgrounds", "bkg000001.fits")
+            output_dir = os.path.join(sim_params["obs_dir"], "backgrounds", "ap")
             output_file = os.path.join(output_dir, f"bkg000001_ap_mode")
 
         try: 
@@ -193,57 +268,63 @@ class Photometry:
             pass 
 
 
-        #if args["mode == "cumulative":
-        #    output_file += f"_{args["mode}_wsize_{args["window_size}_rad_{args["region_radius}.csv"
-        #elif args["mode == "windowed":
         output_file += f"_windowed-mode_integration_{integration}_"
         output_file += self.get_random_string(15)+".csv"
 
         phm = Photometrics({ 'events_filename': input_file })
         region = {
-            'ra': args["pointing"][0],
-            'dec': args["pointing"][1],
+            'ra': photometry_params["pointing"][0],
+            'dec': photometry_params["pointing"][1],
         }
 
         # print('File: ', input_file)
-        print('\nRegion center ', region, 'with radius', args["region_radius"], 'deg')
+        # print('\nRegion center ', region, 'with radius', photometry_params["region_radius"], 'deg')
         
         total = 0
-        if os.path.isfile(output_file) and args["override"] == 0:
+        if os.path.isfile(output_file) and photometry_params["override"] == 0:
             print(f"File {output_file} already exists!")
         else:
 
 
             if integration == "t":
+                windows = time_windows
                 with open(output_file, "w") as of:
                     of.write("VALMIN,VALMAX,VALCENTER,COUNTS,ERROR\n")    
-                    for time_window in time_windows:
-                        region_count = phm.region_counter(region, args["region_radius"], tmin=time_window[0], tmax=time_window[1])
+                    for window in windows:
+                        region_count = phm.region_counter(region, photometry_params["region_radius"], tmin=window[0], tmax=window[1], emin=sim_params["e_window_start"], emax=sim_params["e_window_stop"])
                         total += region_count
-                        # print(f'tmin {time_window[0]} tmax {time_window[1]} -> counts: {region_count}')
-                        of.write(f"{time_window[0]},{time_window[1]},{(time_window[1]+time_window[0])/2},{region_count},{sqrt(region_count)}\n")
-                print("Produced: ",output_file)
+                        # print(f'tmin {window[0]} tmax {window[1]} -> counts: {region_count}')
+                        window_center = (window[1]+window[0])/2
+                        of.write(f"{window[0]},{window[1]},{window_center},{region_count},{sqrt(region_count)}\n")
+
+            
             
             elif integration == "e":
+                windows = energy_windows
                 with open(output_file, "w") as of:
                     of.write("VALMIN,VALMAX,VALCENTER,COUNTS,ERROR\n")    
-                    for energy_window in energy_windows:
-                        region_count = phm.region_counter(region, args["region_radius"], emin=energy_window[0], emax=energy_window[1])
-                        total += region_count
-                        of.write(f"{energy_window[0]},{energy_window[1]},{(energy_window[1]+energy_window[0])/2},{region_count},{sqrt(region_count)}\n")
-                print("Produced: ",output_file)
+                    for window in windows:
+                        region_count = phm.region_counter(region, photometry_params["region_radius"], tmin=sim_params["t_window_start"], tmax=sim_params["t_window_stop"], emin=window[0], emax=window[1])
+                        total += round(region_count, 2)
+                        # print(f'tmin {window[0]} tmax {window[1]} -> counts: {region_count}')
+                        window_center = (window[1]+window[0])/2
+                        of.write(f"{window[0]},{window[1]},{window_center},{region_count},{sqrt(region_count)}\n")
+            
 
+            print("Produced: ",output_file)
+
+            """
             elif integration == "t,e":
                 with open(output_file, "w") as of:
                     of.write("TMIN,TMAX,TMID,EMIN,EMAX,EMID,COUNTS,ERROR\n")    
                     for time_window in time_windows:
                         for energy_window in energy_windows:
-                            region_count = phm.region_counter(region, args["region_radius"], tmin=time_window[0], tmax=time_window[1], emin=energy_window[0], emax=energy_window[1])
+                            region_count = phm.region_counter(region, photometry_params["region_radius"], tmin=time_window[0], tmax=time_window[1], emin=energy_window[0], emax=energy_window[1])
                             total += region_count
                             # print(f'tmin {time_window[0]} tmax {time_window[1]} -> counts: {region_count}')
                             of.write(f"{time_window[0]},{time_window[1]},{(time_window[1]+time_window[0])/2},{energy_window[0]},{energy_window[1]},{(energy_window[1]+energy_window[0])/2},{region_count},{sqrt(region_count)}\n")
                 print("Produced: ",output_file)
-
+            """
         return output_file
                 
 
