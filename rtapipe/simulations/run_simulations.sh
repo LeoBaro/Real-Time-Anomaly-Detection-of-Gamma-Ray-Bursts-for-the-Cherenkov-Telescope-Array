61823506391fa52cac7cd105c569d91564ac2335
LeoBaro <leonardo.baroncelli@inaf.it>
2021-10-18 14:09:30 +0200

    simulations script updated

--- rtapipe/simulations/run_simulations.sh
+++ rtapipe/simulations/run_simulations.sh
@@ -1,10 +1,70 @@
 #!/bin/bash
 
+function createConfigTemplate {
+    if [[ -z "${1}" ]]; then
+        printf "Error: no arguments have been passed to the function. Expected: directory to store the configuration file\n"
+        echo "" 
+    else
+        configFilePath="$1/config.yaml"
+
+        echo " 
+setup:
+    simtype: XXX                      # grb -> src+bkg; bkg -> empty fields; skip -> skip sim; 
+                                    # wobble -> LST-like runs (str)
+    runid: run0406_ID000126           # can be all or any template (list or str) in catalog
+    trials: XXX                       # realisations per runid
+    start_count: XXX                  # starting count for seed (int)
+    scalefluxfactor: XXX                # scale src nominal flux by factor (float)
+
+simulation:
+    caldb: prod3b                     # calibration database
+    irf: South_z40_average_LST_30m    # istrument response function
+    tobs: XXX                         # total obs time (s)
+    onset: XXX                        # time of bkg only a.k.a. delayed onset of burst (s)
+    delay: 0                         # delayed start of observation (s) (float)
+    emin: XXX                        # simulation minimum energy (TeV)
+    emax: XXX                        # simulation maximum energy (TeV)
+    roi: XXX                          # region of interest radius (deg)
+    offset: 0                      # 'gw' -> from alert; value -> otherwise (deg) (str/float)
+    nruns:                        # numer of runs (of lenght=tobs) for wobble simtype (int)
+
+
+analysis:
+    skypix:                       # pixel size in skymap (deg) (float)
+    skyroifrac:                   # ratio between skymap axis and roi (float)
+    smooth:                       # Gaussian corr. kernel rad. (deg) (float)
+    maxsrc:                       # number of hotspot to search for (float)
+    sgmthresh: 3                  # blind-search acc. thresh. in Gaussian sigma (float)
+    usepnt: yes                   # use pointing for RA/DEC (bool)
+    exposure:                     # exposure times for the analysis (s) (float)
+    binned: no                    # perform binned or unbinned analysis (bool)
+    blind: yes                    # requires blind-search (bool)
+    tool: ctools                  # which science tool (str) 
+    type: 3d                      # 1d on/off or 3d full-fov (str)
+    cumulative: no
+    lightcurve: no
+    index: -2.1
+
+options:
+    set_ebl: True                     # uses the EBL absorbed template
+    extract_data: True                # if True extracts lightcurves and spectra 
+    plotsky:                      # if True generates skymap plot (bool)
+
+path: 
+    data: $DATA                       # all data should be under this folder
+    ebl: $DATA/ebl_tables/gilmore_tau_fiducial.csv
+    model: $DATA/models
+    merger: $DATA/mergers                     # folder of alerts (str)
+    bkg: $DATA/models/CTAIrfBackground.xml    # file of background model (str)
+    catalog: $DATA/templates/grb_afterglow/GammaCatalogV1.0
+        " > $configFilePath
+        echo $configFilePath
+    fi
+}
+
 function getConfiguration {
-    #for i; do 
-    #  echo $i 
-    #done  
-    DIR="$1"
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    CONFIG_FILE_TEMPLATE="$1"
     LOG_ID="$2"
     SIMTYPE="$3"
     LOG_DIR="$DIR/conf/temp_$LOG_ID"
@@ -19,7 +79,7 @@ function getConfiguration {
 
     mkdir -p "$LOG_DIR"
 
-    sed "s/simtype: XXX/simtype: $SIMTYPE/g"  $DIR/config.yaml > $LOG_DIR/config.yaml
+    sed "s/simtype: XXX/simtype: $SIMTYPE/g"  $CONFIG_FILE_TEMPLATE > $LOG_DIR/config.yaml
     sed "s/onset: XXX/onset: $ONSET/g" -i $LOG_DIR/config.yaml
     sed "s/tobs: XXX/tobs: $TOBS/g" -i $LOG_DIR/config.yaml
     sed "s/trials: XXX/trials: $TRIALS/g" -i $LOG_DIR/config.yaml
@@ -31,89 +91,53 @@ function getConfiguration {
 
     configFile="$LOG_DIR/config.yaml"
 
-    echo $configFile # this is the return of the function
+    echo "$configFile"
+}
+
+function startSimulation {
+    if [[ -z "${1}" ]]; then
+        printf "Error: no arguments have been passed to the function. Expected: the path to the configuration file\n"
+        echo "" 
+    else
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+        printf "Starting simulations..please wait..\n"
+        current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+        logFile="$DIR/logs/simGRBcatalog_$current_time.log"
+        nohup python ~/Workspace/inaf/phd/cta-sag-sci/RTAscience/simBkg.py -f "$1" --mp-enabled "true" --mp-threads 50 2>&1 > "$logFile" &
+    fi
+}
+
+function message {
+    printf "${GREEN}\n${1}${NORMC}\n";
 }
 
 
 if [[ -z "${DATA}" ]]; then
-    echo "DATA is undefined. Please, export DATA=.."
+    
+    printf "DATA is undefined. Please, export DATA=..\n"
+
 else
 
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-    echo " 
-    setup:
-        simtype: XXX                      # grb -> src+bkg; bkg -> empty fields; skip -> skip sim; 
-                                          # wobble -> LST-like runs (str)
-        runid: run0406_ID000126           # can be all or any template (list or str) in catalog
-        trials: XXX                       # realisations per runid
-        start_count: XXX                  # starting count for seed (int)
-        scalefluxfactor: XXX                # scale src nominal flux by factor (float)
-
-    simulation:
-        caldb: prod3b                     # calibration database
-        irf: South_z40_average_LST_30m    # istrument response function
-        tobs: XXX                         # total obs time (s)
-        onset: XXX                        # time of bkg only a.k.a. delayed onset of burst (s)
-        delay: 0                         # delayed start of observation (s) (float)
-        emin: XXX                        # simulation minimum energy (TeV)
-        emax: XXX                        # simulation maximum energy (TeV)
-        roi: XXX                          # region of interest radius (deg)
-        offset: 0                      # 'gw' -> from alert; value -> otherwise (deg) (str/float)
-        nruns:                        # numer of runs (of lenght=tobs) for wobble simtype (int)
-
-
-    analysis:
-        skypix:                       # pixel size in skymap (deg) (float)
-        skyroifrac:                   # ratio between skymap axis and roi (float)
-        smooth:                       # Gaussian corr. kernel rad. (deg) (float)
-        maxsrc:                       # number of hotspot to search for (float)
-        sgmthresh: 3                  # blind-search acc. thresh. in Gaussian sigma (float)
-        usepnt: yes                   # use pointing for RA/DEC (bool)
-        exposure:                     # exposure times for the analysis (s) (float)
-        binned: no                    # perform binned or unbinned analysis (bool)
-        blind: yes                    # requires blind-search (bool)
-        tool: ctools                  # which science tool (str) 
-        type: 3d                      # 1d on/off or 3d full-fov (str)
-        cumulative: no
-        lightcurve: no
-        index: -2.1
-
-    options:
-        set_ebl: True                     # uses the EBL absorbed template
-        extract_data: True                # if True extracts lightcurves and spectra 
-        plotsky:                      # if True generates skymap plot (bool)
-
-    path: 
-        data: $DATA                       # all data should be under this folder
-        ebl: $DATA/ebl_tables/gilmore_tau_fiducial.csv
-        model: $DATA/models
-        merger: $DATA/mergers                     # folder of alerts (str)
-        bkg: $DATA/models/CTAIrfBackground.xml    # file of background model (str)
-        catalog: $DATA/templates/grb_afterglow/GammaCatalogV1.0
-    " > $DIR/config.yaml
-
-
-
-    log_id=0
-
     mkdir -p $DIR/logs
 
-    # onset - tobs - trials - start count
 
-    #cfgfile=$(getConfiguration $DIR $log_id bkg 0 1800 100 1 0.03 0.15 0.5 1)
-    #echo "Generated: $cfgfile"
-    #echo "Calling simBkg for bkg simulations..please wait!"
-    # call me with nohup <> & 
-    #nohup python ~/Workspace/inaf/phd/cta-sag-sci/RTAscience/simBkg.py -f $cfgfile --mp-enabled "false" --mp-threads 35 2>&1 > "$DIR/logs/simGRBcatalog_bkg.log" &
-    #log_id=$((log_id+1))
+    message "Creating configuration file template"    
+    configFileTemplate=$(createConfigTemplate $DIR)  
+    printf "Configuration file template=$configFileTemplate\n"
 
-    cfgfile=$(getConfiguration $DIR $log_id grb 900 1800 100 100 0.03 0.15 0.5 2)
-    echo "Generated: $cfgfile"
-    echo "Calling simGRBcatalog for grb simulations..please wait!"
-    # call me with nohup <> & 
-    nohup python ~/Workspace/inaf/phd/cta-sag-sci/RTAscience/simGRBcatalog.py -f $cfgfile --mp-enabled "true" --mp-threads 30 2>&1 > "$DIR/logs/simGRBcatalog_grb.log" &
+    message "BKG simulations"
+    log_id=0
+    configFile=$(getConfiguration "$configFileTemplate" "$log_id" "bkg" 0   1800 100 1   0.03 0.15 0.5 1)
+    printf "Configuration file=$configFile\n"
+    
+    message "GRB simulations"
     log_id=$((log_id+1))
+    configFile=$(getConfiguration "$configFileTemplate" "$log_id" "grb" 900 1800 100 100 0.03 0.15 0.5 2)
+    printf "Configuration file=$configFile\n"
+    # TODO startSimulation
+
+
 
     rm $DIR/config.yaml
 fi
