1ed1f1d57de4aff9528073e06aeab04c12dd7aaa
LeoBaro <leonardo.baroncelli@inaf.it>
2021-10-22 12:44:55 +0200

    updates

--- rtapipe/analysis/dataset/generate_for_training.sh
+++ rtapipe/analysis/dataset/generate_for_training.sh
@@ -2,13 +2,14 @@
 
 if [ $# -lt 2 ]; 
     then
-        printf "\nNot enought arguments supplied.\nArguments: \n\t- integration time\n\t- lenght of the training time series sample.\n\n"
+        printf "\nNot enought arguments supplied.\nArguments: \n\t- simulation dataset \n\t- integration time\n\t- lenght of the training time series sample.\n\n"
     else
 
         dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
         today=`date +%Y-%m-%d.%H:%M:%S`
-        it=$1
-        tsl=$2
+        simdata=$1
+        it=$2
+        tsl=$3
         outdir="${dir}/ap_data_for_training_date_${today}"
 
         printf "Generating training set\n"
@@ -18,7 +19,7 @@ if [ $# -lt 2 ];
         printf "\toutputdir=$outdir\n"
 
         python generate_ap_data.py \
-                -dd $DATA/obs/simtype_bkg_os_0_tobs_1800_irf_South_z40_average_LST_30m_emin_0.03_emax_1.0_roi_2.5 \
+                -dd $DATA/obs/$simdata \
                 -t bkg \
                 -mp no \
                 -it $it \
