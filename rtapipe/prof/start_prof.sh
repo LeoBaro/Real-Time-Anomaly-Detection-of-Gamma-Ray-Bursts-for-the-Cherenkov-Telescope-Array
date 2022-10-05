03102b75b7dd835e1aa57b56c8be57d4c463d75f
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-10-05 22:45:27 +0200

    Update script

--- rtapipe/prof/start_prof.sh
+++ rtapipe/prof/start_prof.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m cProfile -o sim_bkg.prof ../../cta-sag-sci/RTAscience/simBkg.py -f ./config_trials_1-10.yaml -out ./output
+#python -m cProfile -o sim_bkg.prof ../../cta-sag-sci/RTAscience/simBkg.py -f ./config_trials_1-10.yaml -out ./output
 
-python -m cProfile -o sim_grb_catalog_with_randomization.prof python ../../cta-sag-sci/RTAscience/simGRBcatalogWithRandomization.py -f config.yml --output-dir ./output --print no --mp-threads 30
+python3 -m cProfile -o sim_grb_catalog_with_randomization.prof ../../cta-sag-sci/RTAscience/simGRBcatalogWithRandomization.py -f config.yml --output-dir ./output --print no --mp-threads 30
