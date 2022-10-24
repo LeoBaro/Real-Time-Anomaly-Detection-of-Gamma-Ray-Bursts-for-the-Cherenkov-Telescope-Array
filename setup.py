b53f072e9eb5b9d4d085b8578be5a98606231b02
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-10-24 16:57:53 +0200

    Add entrypoint script

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ entry_points = {
 	'console_scripts': [
 		'plot_ap_timeseries = rtapipe.scripts.plots.plot_ap_timeseries:main',
           'generate_ap_data = rtapipe.lib.dataset.generate_ap_data:main',
-          'phlists_to_skymaps = rtapipe.scripts.plots.phlists_to_skymaps:main'
+          'phlists_to_skymaps = rtapipe.scripts.plots.phlists_to_skymaps:main',
+          'get_dataset_config_path = rtapipe.lib.dataset.config.get_dataset_config_path:main'
      ]
 }
 setup( 
