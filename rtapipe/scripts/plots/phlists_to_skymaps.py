5b4df908fce067c78af896ee891ef596c4bb15fd
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2022-11-03 10:50:33 +0100

    Add script to generate skymaps from dl3 with regions

--- rtapipe/scripts/plots/phlists_to_skymaps.py
+++ rtapipe/scripts/plots/phlists_to_skymaps.py
@@ -1,17 +1,30 @@
 import os
 import argparse
-from RTAscience.lib.RTACtoolsAnalysis import RTACtoolsAnalysis
-from RTAscience.lib.RTAVisualise import  plotSkymap
+from pathlib import Path
+from RTAscience.cfg.Config import Config
+from sagsci.tools.plotting import SkyImage
+from RTAscience.lib.RTAUtils import get_pointing
+from rtapipe.lib.datasource.Photometry3 import OnlinePhotometry
 
 def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--data-dir', type=str, required=True)
+    parser.add_argument('--conf-file', type=str, required=True)    
     parser.add_argument('--subtraction', type=str, choices=["NONE","IRF"], required=True)
     args = parser.parse_args()
 
-    rta = RTACtoolsAnalysis()
-    rta.usepnt = True
-    rta.sky_subtraction = args.subtraction
+    cfg = Config(args.conf_file)
+
+    #rta.caldb = cfg.get('caldb')
+    #rta.irf = cfg.get('irf')
+
+    template =  Path(os.environ["DATA"]).joinpath("templates",f"{cfg.get('runid')}.fits")
+    target = get_pointing(template) 
+    
+    pht = OnlinePhotometry(args.conf_file)
+    regions_conf = pht.create_photometry_configuration(region_radius=0.2, number_of_energy_bins=3, rings_n=1, flatten=True)
+    regions = [r[0] for r in regions_conf]
+    # print(regions)
 
     for root, subdirs, files in os.walk(args.data_dir):
 
@@ -19,20 +32,22 @@ def main():
 
         for filename in fits_files: 
 
-            rta.input = os.path.join(root, filename)
-            rta.output = rta.input.replace(".fits",f"_sub_{rta.sky_subtraction}.skymap.fits")
-            # Add more parameters here
-            # ..
-
-
-            print(f"Producing.. {rta.output}")
-
-            if not os.path.isfile(rta.output):
-                rta.run_skymap(wbin=0.02)
-            else:
-                print(f"File {rta.output} already exists!")
-                
-            plotSkymap(rta.output, show=False, usetex=False, png=root)
+            plot = SkyImage()
+            plot.set_target(ra=target[0], dec=target[1])
+            plot.set_pointing(ra=target[0], dec=target[1])
+            img = os.path.join(root, filename)
+            out = img.replace('.fits', '.png')
+            print(f"Producing.. {out}")
+            plot.counts_map_with_regions(
+                    img, 
+                    regions, 
+                    trange=None, 
+                    erange=[cfg.get('emin'), cfg.get('emax')], 
+                    roi=cfg.get('roi'), 
+                    name=out, 
+                    title="Skymap", 
+                    )
+            del plot
         
 
 if __name__=="__main__":
