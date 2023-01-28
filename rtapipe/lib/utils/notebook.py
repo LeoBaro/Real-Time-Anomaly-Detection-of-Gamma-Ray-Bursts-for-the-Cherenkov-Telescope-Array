4ce2cbc7d20243ceba7d4ff5581c1333e14143d8
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2023-01-28 23:55:06 +0100

    Required file

---
+++ rtapipe/lib/utils/notebook.py
@@ -0,0 +1,284 @@
+import yaml
+import pickle
+import numpy as np 
+import pandas as pd 
+from pathlib import Path
+from rtapipe.lib.utils.misc import dotdict
+from rtapipe.lib.models.anomaly_detector_builder import AnomalyDetectorBuilder
+from rtapipe.lib.evaluation.pval import get_pval_table, get_threshold_for_sigma
+
+def load_model(model_id):
+    with open("./trained_models.yaml", "r") as f:
+        try:
+            configs = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(exc)
+    model_config = [c for c in configs["models"] if c["id"] == model_id]
+    model_config = dotdict(model_config.pop())
+    model_config.ad = AnomalyDetectorBuilder.getAnomalyDetector(name=model_config.name, timesteps=model_config.timesteps, nfeatures=model_config.nfeatures, load_model="True", training_epoch_dir=model_config.path, training=False)
+    model_config.pvalue_table = get_pval_table(model_config.pval_path) 
+    return model_config
+
+def filter_templates(templates_detections, model, other_model, which, sigma, detection_index_max):
+
+    templates = []
+    
+    for template_name, template_result in templates_detections.items():
+    
+        model_detected       = template_result[model][f"{sigma}s_detection"]
+        
+        if which == "common":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if model_detected and other_model_detected:
+                model_first_detection_index       = template_result[model][f"{sigma}s_detections_indexes"][0]
+                other_model_first_detection_index = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index <= detection_index_max) and (other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)
+
+        elif which == "first":
+            if model_detected:
+
+                model_first_detection_index       = template_result[model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+
+                    
+        elif which == "first-only":
+
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+
+            if model_detected and (not other_model_detected):
+
+                model_first_detection_index       = template_result[model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+        
+
+        elif which == "second":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if other_model_detected:
+
+                other_model_first_detection_index       = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+
+                    
+        elif which == "second-only":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if (not model_detected) and other_model_detected:
+
+                other_model_first_detection_index       = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+                
+        
+        
+        elif which == "none":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if (not model_detected) and (not other_model_detected):
+                templates.append(template_name)
+
+    
+        #model_first_detection_index      = template_result[model][f"{sigma}s_detections_indexes"][0]
+        #othermodel_first_detection_index = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+    return templates
+
+def get_first_detections(templates_detections, model, sigma):
+    first_detection_indexes = []
+    for template_result in templates_detections.values():
+            if len(template_result[model][f"{sigma}s_detections_indexes"]) > 0:
+                first_detection_indexes.append(template_result[model][f"{sigma}s_detections_indexes"][0])
+    return np.array(sorted(first_detection_indexes))
+
+def get_detections_in_time(templates_detections, model, tmax, onset_index, sigma, integration_time):
+    first_detection_indexes = []
+    for template_result in templates_detections.values():
+        if len(template_result[model][f"{sigma}s_detections_indexes"]) > 0:
+            first_detection_indexes.append(template_result[model][f"{sigma}s_detections_indexes"][0])
+    detection_time = (np.array(first_detection_indexes)-onset_index)*integration_time
+    dt_filterd = [dt for dt in detection_time if dt <= tmax]
+    return len(dt_filterd)
+
+
+def get_dd(model, templates_detections, sigma, onset_index, integration_time, templates):
+    first_detection_indexes = []
+
+    for template_name in templates:
+
+            first_detection_index = templates_detections[template_name][model][f"{sigma}s_detections_indexes"][0]
+            
+            first_detection_indexes.append(first_detection_index)
+
+    first_detection_time_relative_to_tt = (np.array(first_detection_indexes)-onset_index)*integration_time
+
+    mean_dd = first_detection_time_relative_to_tt.mean()
+    std_dd = first_detection_time_relative_to_tt.std()
+    
+    return round(mean_dd,2), round(std_dd,2)
+
+ 
+def crop_to_5(s):
+    if s > 5: 
+        s = 5.00001
+    return s
+
+def get_sigma_table(file_path, fix_independence=False, bins_temporal_size=None):
+    sigma_table = pd.read_csv(file_path, index_col=0)
+    if fix_independence:
+        for time_bin in sigma_table.index:
+            if int(time_bin.split("-")[1])%bins_temporal_size != 0:
+                sigma_table.loc[time_bin] = 0
+
+    sigma_table = sigma_table.applymap(crop_to_5)
+    return sigma_table
+
+def get_templates_detections(rnn_st, cnn_st, lima_st):
+
+    templates_detections = dotdict({})
+    
+    for templ_det in lima_st:
+        templates_detections[templ_det] = dotdict({}) 
+        templates_detections[templ_det]["li_ma"] = dotdict({})
+        templates_detections[templ_det]["cnn"] = dotdict({})
+        templates_detections[templ_det]["rnn"] = dotdict({})
+
+    for templ_det in lima_st:
+        detections_3s = lima_st[templ_det] >= 3
+        detections_5s = lima_st[templ_det] >= 5
+        templates_detections[templ_det]["li_ma"]["3s_detection"] = detections_3s.any()
+        templates_detections[templ_det]["li_ma"]["5s_detection"] = detections_5s.any()
+        templates_detections[templ_det]["li_ma"]["3s_detections_indexes"] = np.where(detections_3s == True)[0]
+        templates_detections[templ_det]["li_ma"]["5s_detections_indexes"] = np.where(detections_5s == True)[0]
+
+    for templ_det in rnn_st:
+        detections_3s = rnn_st[templ_det] >= 3
+        detections_5s = rnn_st[templ_det] >= 5
+        templates_detections[templ_det]["rnn"]["3s_detection"] = detections_3s.any()
+        templates_detections[templ_det]["rnn"]["5s_detection"] = detections_5s.any()
+        templates_detections[templ_det]["rnn"]["3s_detections_indexes"] = np.where(detections_3s == True)[0]
+        templates_detections[templ_det]["rnn"]["5s_detections_indexes"] = np.where(detections_5s == True)[0]
+        
+    for templ_det in cnn_st:
+        detections_3s = cnn_st[templ_det] >= 3
+        detections_5s = cnn_st[templ_det] >= 5
+        templates_detections[templ_det]["cnn"]["3s_detection"] = detections_3s.any()
+        templates_detections[templ_det]["cnn"]["5s_detection"] = detections_5s.any()
+        templates_detections[templ_det]["cnn"]["3s_detections_indexes"] = np.where(detections_3s == True)[0]
+        templates_detections[templ_det]["cnn"]["5s_detections_indexes"] = np.where(detections_5s == True)[0] 
+
+    return templates_detections
+
+
+def filter_templates(templates_detections, model, other_model, which, sigma, detection_index_min, detection_index_max):
+
+    templates = []
+    
+    for template_name, template_result in templates_detections.items():
+    
+        model_detected       = template_result[model][f"{sigma}s_detection"]
+        
+        if which == "common":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if model_detected and other_model_detected:
+                model_first_detection_index       = template_result[model][f"{sigma}s_detections_indexes"][0]
+                other_model_first_detection_index = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index >= detection_index_min and model_first_detection_index <= detection_index_max) and (other_model_first_detection_index >= detection_index_min and other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)
+
+        elif which == "first":
+            if model_detected:
+
+                model_first_detection_index = template_result[model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index >= detection_index_min and model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+
+                    
+        elif which == "first-only":
+
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+
+            if model_detected and (not other_model_detected):
+
+                model_first_detection_index       = template_result[model][f"{sigma}s_detections_indexes"][0]
+                
+                if (model_first_detection_index >= detection_index_min and model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+        
+
+        elif which == "second":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if other_model_detected:
+
+                other_model_first_detection_index       = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (other_model_first_detection_index >= detection_index_min and other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+
+                    
+        elif which == "second-only":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if (not model_detected) and other_model_detected:
+
+                other_model_first_detection_index       = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+                
+                if (other_model_first_detection_index >= detection_index_min and other_model_first_detection_index <= detection_index_max): 
+                    templates.append(template_name)                
+                
+        
+        
+        elif which == "none":
+            other_model_detected = template_result[other_model][f"{sigma}s_detection"]
+            if (not model_detected) and (not other_model_detected):
+                templates.append(template_name)
+
+    
+        #model_first_detection_index      = template_result[model][f"{sigma}s_detections_indexes"][0]
+        #othermodel_first_detection_index = template_result[other_model][f"{sigma}s_detections_indexes"][0]
+    return templates
+
+
+def evaluate_metrics(model_config, test_all_x, test_all_y, output_dir, filename, additional_tresholds=[]):
+    output_file = Path(output_dir).joinpath(filename)
+    if output_file.exists():
+        print(f"Loading {filename} from {output_file}...")
+        with open(output_file, "rb") as f:
+            results = pickle.load(f)
+        return results
+
+    results = {
+        "threshold" : [],
+        "accuracy" : [],
+        "precision" : [],
+        "recall" : [],
+        "false_positive_rate" : []
+    }
+    for sigma_threshold in additional_tresholds:
+        model_config.ad.threshold = sigma_threshold
+        metrics = model_config.ad.evaluate(test_all_x, test_all_y)
+        results["threshold"] += [sigma_threshold]
+        for key, value in metrics.items():
+            if key in ["accuracy", "precision", "recall", "false_positive_rate"]:
+                results[key] += [value]
+
+    for sigma_thresh in range(1,6):
+        sigma_threshold = get_threshold_for_sigma(model_config.pvalue_table, sigma_thresh)
+        model_config.ad.threshold = sigma_threshold
+        #print(f"Threshold: {model_config_rnn.ad.threshold} corresponding to {get_sigma_from_ts(model_config_rnn.pvalue_table, model_config_rnn.ad.threshold)} sigma")
+        metrics = model_config.ad.evaluate(test_all_x, test_all_y)
+        results["threshold"] += [sigma_threshold]
+        for key, value in metrics.items():
+            if key in ["accuracy", "precision", "recall", "false_positive_rate"]:
+                results[key] += [value]
+ 
+    with open(output_file, "wb") as f:
+        print("Generating output file: ", output_file)
+        pickle.dump(results, f)
+
+    return results
